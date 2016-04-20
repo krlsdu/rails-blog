aaf4c92247b214b585fbb40382781ae19747cd7a
krlsdu <krlsdu@gmail.com>
2016-04-20 00:10:13 -0300

    Testing id travis not deploy app fail

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe WelcomeController, type: :controller do
   describe "GET index" do
     it "say 'Hello World' " do
     get :index
-    expect(response.body).to match /Hello/
+    expect(response.body).to match /"carlos"/
 
     end
 

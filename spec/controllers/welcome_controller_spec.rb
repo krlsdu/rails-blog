c5686c9939a6f03c1330f0b25826b4f4843c04c7
krlsdu <krlsdu@gmail.com>
2016-04-22 12:04:10 -0300

    Fix test

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe WelcomeController, type: :controller do
   describe "GET index" do
     it "say 'Hello World' " do
     get :index
-    expect(response.body).to match /"carlos"/
+    expect(response.body).to match /Hello World/
 
     end
 
